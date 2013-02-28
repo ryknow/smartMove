9fcbb3ba7c4023d0325237e05c736b3ec41a64ac
ryknow <ryanknowles@gmail.com>
2013-02-27 21:50:43 -0500

    updated project to Maven

--- adeptStratagem/src/Main.java
+++
@@ -1,8 +0,0 @@
-
-public class Main {
-
-    public static void main(String[] args) {
-        File jpegFile = new File("myImage.jpg");
-        Metadata metadata = ImageMetadataReader.readMetadata(jpegFile);
-    }
-}
